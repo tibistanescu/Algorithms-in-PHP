7cd827b45c51a34a2d7f7b9cb3b1f613594219be
Tiberiu <stanescutiberiugabriel@gmail.com>
2018-04-04 15:24:44 +0300

    Update reverse_string.php

--- reverse_string.php
+++ reverse_string.php
@@ -1,2 +1,19 @@
 <?php
+/*
+ *  Problem:
+ *  Given a string, return a new string with the reversed order of characters
+ *  Examples:
+ *  reverse('banana') === 'ananab'
+ *  reverse('hello') === 'olleh'
+ *  reverse('World!') === '!dlroW'
+ */
+function reverse($str)
+{
+    $reversedString = '';
 
+    foreach (str_split($str) as $character) {
+        $reversedString = $character . $reversedString;
+    }
+
+    return $reversedString;
+}
